62fa6e0ed4a36a6d26ca35213bd4a8577fa7ede5
Knight-4000 <yorksprinters@gmail.com>
2023-05-11 10:17:51 -0400

    Updated firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "REACT_APP_API_KEY",
-  authDomain: "REACT_APP_AUTH_DOMAIN",
-  projectId: "REACT_APP_PROJECT_ID",
-  storageBucket: "REACT_APP_BUCKET",
-  messagingSenderId: "REACT_APP_SENDER_ID",
-  appId: "REACT_APP_APP_ID"
+  apiKey: "process.env.REACT_APP_API_KEY",
+  authDomain: "process.env.REACT_APP_AUTH_DOMAIN",
+  projectId: "process.env.REACT_APP_PROJECT_ID",
+  storageBucket: "process.env.REACT_APP_BUCKET",
+  messagingSenderId: "process.env.REACT_APP_SENDER_ID",
+  appId: "process.env.REACT_APP_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
